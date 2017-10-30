f0394ff07ad6fa2517e32633fa926ea957f46d49
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2017-10-30 09:43:00 +0300

    les 1

---
+++ home.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//import welcome from './welcome';
+
+//welcome("home");
+
+//exports.welcome = welcome;
+//
+//
+
+let welcome = require('./welcome.js');
+
+welcome('home');
+
---
+++ welcome.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//export default function(message) {
+
+  //if (NODE_ENV == 'development') {
+    //console.log(message);
+  //}
+
+  //alert(`Welcome ${message}`);
+//};
+
+module.exports = function(message) {
+    alert(`Welcome ${message}`);
+};
